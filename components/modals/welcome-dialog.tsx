b7f3de12b642e8ef93eaa112af2e114cc25b310a
Lachie <lachie.duncan12@gmail.com>
2024-08-04 10:44:01 +1000

    fixed appearance of welcome modal on mobile

--- components/modals/welcome-dialog.tsx
+++ components/modals/welcome-dialog.tsx
@@ -10,6 +10,7 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface WelcomeDialogProps {
     openRef: React.RefObject<HTMLButtonElement>
@@ -36,27 +37,29 @@ export default function WelcomeDialog({ openRef }: WelcomeDialogProps) {
                     </DialogTitle>
                 </DialogHeader>
 
-                <p>Hello! 👋</p>
-                <p>Welcome to Pocket Adviser! I am your AI stock market copilot, here to assist you on your investing journey.</p>
-                <p>I can help you find investments that fit your objectives and preferences, explain financial concepts, and navigate market events.</p>
-                <p>Get started in <b>three easy steps</b>.</p>
+                <ScrollArea className='max-h-[75vh] sm:max-h-[80vh]'>
+                    <p>Hello! 👋</p>
+                    <p>Welcome to Pocket Adviser! I am your AI stock market copilot, here to assist you on your investing journey.</p>
+                    <p>I can help you find investments that fit your objectives and preferences, explain financial concepts, and navigate market events.</p>
+                    <p>Get started in <b>three easy steps</b>.</p>
 
-                <div className='grid grid-cols-1 sm:grid-cols-3 place-items-center gap-2 my-3'>
-                    <div className='w-[280px] sm:w-auto flex flex-row sm:flex-col items-center gap-3.5 md:gap-2 px-2 py-3 border-2 border-slate-200 rounded-xl'>
-                        <ClipboardList size={48} strokeWidth={1.5} />
-                        <span className='font-medium text-left sm:text-center'>1. Adjust your profile</span>
-                    </div>
+                    <div className='grid grid-cols-1 sm:grid-cols-3 place-items-center gap-2 my-3'>
+                        <div className='w-full max-w-[280px] sm:w-auto sm:max-w-max flex flex-row sm:flex-col items-center gap-3.5 md:gap-2 px-2 py-3 border-2 border-slate-200 rounded-xl'>
+                            <ClipboardList strokeWidth={1.5} className='h-full w-auto min-w-6' />
+                            <span className='font-medium text-left sm:text-center'>1. Adjust your profile</span>
+                        </div>
 
-                    <div className='w-[280px] sm:w-auto flex flex-row sm:flex-col items-center gap-3.5 md:gap-2 px-2 py-3 border-2 border-slate-200 rounded-xl'>
-                        <CircleFadingPlus size={48} strokeWidth={1.5} />
-                        <span className='font-medium text-left sm:text-center'>2. Add stocks to your portfolio</span>
-                    </div>
+                        <div className='w-full max-w-[280px] sm:w-auto sm:max-w-max flex flex-row sm:flex-col items-center gap-3.5 md:gap-2 px-2 py-3 border-2 border-slate-200 rounded-xl'>
+                            <CircleFadingPlus strokeWidth={1.5} className='h-full w-auto min-w-6' />
+                            <span className='font-medium text-left sm:text-center'>2. Add stocks to your portfolio</span>
+                        </div>
 
-                    <div className='w-[280px] sm:w-auto flex flex-row sm:flex-col items-center gap-3.5 md:gap-2 px-2 py-3 border-2 border-slate-200 rounded-xl'>
-                        <MessageCircleMore size={48} strokeWidth={1.5} />
-                        <span className='font-medium text-left sm:text-center'>3. Start a discussion!</span>
+                        <div className='w-full max-w-[280px] sm:w-auto sm:max-w-max flex flex-row sm:flex-col items-center gap-3.5 md:gap-2 px-2 py-3 border-2 border-slate-200 rounded-xl'>
+                            <MessageCircleMore strokeWidth={1.5} className='h-full w-auto min-w-6' />
+                            <span className='font-medium text-left sm:text-center'>3. Start a discussion!</span>
+                        </div>
                     </div>
-                </div>
+                </ScrollArea>
 
                 <DialogClose asChild>
                     <Button>
